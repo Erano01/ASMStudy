906d5c1b00c8437c2eb633aec41e766829506f73
Erano <muhammedalisert01@gmail.com>
2026-01-30 04:41:26 +0300

    Feat: commenting build.bat

--- program001/build.bat
+++ program001/build.bat
@@ -1,6 +1,11 @@
 @echo off
 echo Assembly programi derleniyor (MASM x64)...
 
+REM Aşağıda kod derleniyor /c ile. /Zi hata ayıklama bilgisi ekleniyor.
+REM ML64 komutu MASM x64 derleyicisidir. 
+REM hello.asm dosyasını derleyip hello.obj dosyasını oluşturur.
+REM /c stands for "Compile the assembly source file"
+REM Compiling != Assembling.
 ml64 /c /Zi hello.asm
 if errorlevel 1 (
     echo HATA: ML64 derleme hatasi!
@@ -8,6 +13,8 @@ if errorlevel 1 (
     exit /b 1
 )
 
+REM Aşağıda obj dosyası linkleniyor ve çalıştırılabilir dosya oluşturuluyor.
+REM link komutu obj dosyasını alır ve hello.exe dosyasını oluşturur.
 link hello.obj /SUBSYSTEM:CONSOLE /ENTRY:main /OUT:hello.exe kernel32.lib ucrt.lib vcruntime.lib msvcrt.lib legacy_stdio_definitions.lib
 if errorlevel 1 (
     echo HATA: Link hatasi!
